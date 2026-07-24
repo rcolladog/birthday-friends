7cdead6e62d2de8073fb0a5e7304480404ddbc77
rutskai <ruth3579@gmail.com>
2026-07-24 06:45:30 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,16 @@
 import { Ionicons } from '@expo/vector-icons';
-import { usePathname } from 'expo-router';
-import { StyleSheet as RNStyleSheet, View } from 'react-native';
+import { router, usePathname } from 'expo-router';
+import { Pressable, StyleSheet as RNStyleSheet, View } from 'react-native';
 
 export default function Footer() {
 const path=usePathname();
 
 return (
       <View style={style.container}>
-       <Ionicons style={path=== "/" ? style.activeIcon : style.icons} name="home-outline" size={24} color="white" />
+        <Pressable onPress={()=> router.push("/")}> 
+            <Ionicons style={path=== "/" ? style.activeIcon : style.icons} name="home-outline" size={24} color="white" />
+        </Pressable>
+      
        <Ionicons style={path=== "" ? style.activeIcon : style.icons} name="calendar-outline" size={24} color="white" />
        <Ionicons style={[path=== "" ? style.activeIcon : style.icons]} name="notifications-outline" size={24} color="white" />
       </View>
@@ -22,7 +25,7 @@ const style= RNStyleSheet.create({
         gap:60, 
         height:90, 
         width:"100%",   
-        backgroundColor:"#242424", 
+        backgroundColor:"#121111", 
         borderTopWidth:1, 
         color:"#242424",
         borderColor:"#4b4a4a"},
