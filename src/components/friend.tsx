fe3e9c5ef838449522cfc38d29a3f3e51bf41901
Ruth <rcolladogarcia@iessonferrer.net>
2026-08-08 06:35:19 +0200

    Add alert on delete Friend

--- src/components/friend.tsx
+++ src/components/friend.tsx
@@ -1,5 +1,11 @@
-import { Image, StyleSheet as RNStyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { Alert, Image, Pressable, StyleSheet as RNStyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 import { FriendInterface } from '../interface/friend';
+import { friendService } from '../services/friendService';
+
 
 
 export default function Friend({ friends }: { friends: FriendInterface[] }) {
@@ -11,8 +17,41 @@ export default function Friend({ friends }: { friends: FriendInterface[] }) {
             .slice(0, 2)
             .map((char) => char.charAt(0).toUpperCase())
             .join("");
-
     };
+      const handleDelete = async (id: string) => {
+    Alert.alert(
+        "Eliminar amigo",
+        "¿Estás seguro de que quieres eliminar este amigo?",
+        [
+            {
+                text: "Cancelar",
+                onPress: () => console.log("Cancelado"),
+                style: "cancel",
+            },
+            {
+                text: "Eliminar",
+                onPress: async () => {
+                    try {
+                        await friendService.remove(id);
+                        router.push("/");
+                    } catch (error) {
+                        console.error("Error eliminando amigo:", error);
+                    }
+                },
+                style: "destructive",
+            },
+        ]
+    );
+};
+
+      const renderRightActions = (id: string) => (
+        <Pressable 
+            style={style.deleteAction}
+            onPress={() => handleDelete(id)}
+        >
+             <Ionicons name="trash" size={24} color="#fff" />
+        </Pressable>
+    );
 
 
 
@@ -27,23 +66,27 @@ export default function Friend({ friends }: { friends: FriendInterface[] }) {
     }
 
     return (
-        <>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             {friends.map((friend) => (
-                <View key={friend.id} style={style.div}>
-                    {friend.image ? (
-                        <Image style={style.image} source={{ uri: friend.image }} />
-                    ) : (
-                        <View style={style.initialsCircle}>
-                            <Text style={style.initialsText}>{getInitial(friend.name)}</Text>
-                        </View>
-                    )}
+                <Swipeable
+                    key={friend.id}
+                    renderRightActions={() => renderRightActions(friend.id)}
+                >
+                    <View style={style.div}>
+                        {friend.image ? (
+                            <Image style={style.image} source={{ uri: friend.image }} />
+                        ) : (
+                            <View style={style.initialsCircle}>
+                                <Text style={style.initialsText}>{getInitial(friend.name)}</Text>
+                            </View>
+                        )}
 
-                    <Text style={style.name}>{friend.name}</Text>
-                    <Text style={style.date}>En {friend.birthday} días</Text>
-                </View>
+                        <Text style={style.name}>{friend.name}</Text>
+                        <Text style={style.date}>En {friend.birthday} días</Text>
+                    </View>
+                </Swipeable>
             ))}
-
-        </>
+        </GestureHandlerRootView>
     );
 }
 
@@ -110,5 +153,16 @@ const style = RNStyleSheet.create({
         fontWeight: "600",
         textAlign: "center",
         marginTop: 20
+    },
+
+     deleteAction: {
+        backgroundColor: "#ff4444",
+        alignItems: "center",
+        justifyContent: "center",
+        width:50,
+        height:50,
+        borderRadius: 50,
+        marginTop:30,
+       marginLeft:30
     },
 });
